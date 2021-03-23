47603cd08c9d0abde816816da8a46dad8ee9b7ee
Itachi <minhthuan1091996@gmail.com>
2021-03-23 21:07:22 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+DataStructure_Week1
---
+++ src/DataStructure_Week1.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : DataStructure_Week1.c
+ Author      : Itachi
+ Version     :
+ Copyright   : 
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	setbuf(stdout, NULL);
+
+	return EXIT_SUCCESS;
+}
