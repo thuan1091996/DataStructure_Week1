40efd798d8c93ef82f1ba1e8f2daaf029d0c357e
Itachi <minhthuan1091996@gmail.com>
2021-04-02 22:52:03 +0700

    [RELEASE] Bracket

--- src/DataStructure_Week1.c
+++ src/DataStructure_Week1.c
@@ -7,28 +7,66 @@
  Description : Hello World in C, Ansi-style
  ============================================================================
  */
-#include "data_structures/lifo_stack.h"
+//#include "data_structures/lifo_stack.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 #include "string.h"
 
-#ifdef TEST
-#define TEST_LL 	0
-#define TEST_STACK 	1
-#endif /* TEST */
+/******************************************************************************
+* Typedefs
+*******************************************************************************/
+typedef struct Mylist
+{
+    void* Key_P;
+    struct Mylist *Next_P;
+}myLL;
+
+typedef struct LLObject
+{
+	/* Attribute */
+	myLL* Head_P;
+	myLL* Tail_P;
+}LLObject_T;
+
+typedef struct myStack
+{
+	LLObject_T*	 	StackLL;
+	uint16_t 		numbElement;
+}StackObj_T;
+/******************************************************************************
+* Variables
+*******************************************************************************/
+
+/******************************************************************************
+* Macros
+*******************************************************************************/
+
+/******************************************************************************
+* Function Prototypes
+*******************************************************************************/
+//StackObj_T* StackCreate(void);
+void StackInit(StackObj_T* Obj_P);
+void StackPush(StackObj_T* Obj_P, uint8_t data);
+uint8_t StackGetTop(StackObj_T* Obj_P);
+uint8_t StackPop(StackObj_T* Obj_P);
+bool StackIsEmpty(StackObj_T* Obj_P);
+
+
+StackObj_T BracketStack;
+StackObj_T PosStack;
+
+
+
+#define TEST 0
 
 #if TEST
-	#include "data_structures/linked_list.h"
-	void TestLL(void);
-	void TestStack(void);
-	char CountChar[]={0,1,2,3,4,5,6,7,8};
-	uint8_t g_checkme=255;
+#define MAX_INPUT 	200000
+char input_data[MAX_INPUT]="{}{}[]{[]}{}[][]{}(){}{}()[][[]]{}{}[]{}[]()[()][](){}{}[]{()}()[][[]]{}[](){{}}[({})][]({})(){}[](())(()){}[][[]]()[][()]{}{[]}{}{}(){}()[][]()()()[()]({}){}(){}[](){}[]()[]{}()[]()[](){}[]({}){}[{}][](){}(())()[[]][()][]()[][]()[][]{}[]()[][]((()))[][](())({})[][]{}()({[]})([])()()[][()][[]](){}{}(())[[]](())()[]()[]()()({})(){()}({})()(){}()()[](([]))()[()][]{}()[()]()([]){}(){{}}{}[()]([])()[]{}{}[](){}[{}](){}[]{[]}[][]()()()({}){}{{{}}}(())[{{}}][({})]{{}}{[]}{}{}({})[][{}]()([])(()){()}()[]{{}}{}((())){}[[{()}]]{}{}({{}}){[]}{}{}{{}}[[{}]][][[()]]()(){(())}([])()[](){}[]()[]{{}}[][][[]]()[][()](())[]()[[]]{}{}[{}]()(){}(){}{}{}[{}][]{()}{{}}{{}}[{}]{}()()(()){}[][{}]{()}{}{[]}{}[]{{}}{{}}{()}(){}()({}){}{}[]([])[]()[][]{()}()[{}][]()[{}]{}(){[]}{}(){[]}(){()}[][([])][{}][()](){()}{}[{}]()()()[]{[]}{{}}{{()}}[{}]{{}}[][]{}({})()[][]()()[[]](())[()]{}[()][][][[]][()][]{}()()[][][{}]()[{}]()[]([{}])[([])]()([])()(){{}}{}()[[]]()(){}([])[]({{}}){}{}[][][]{}[]{}()(){}(){{}}(){}(()){{}}{{}}()[]{}([{}])((()))[()]{}[]{}[]{}()[{}]()[][[{}]]{[]}({})()[{}]{}()[{}][][]([]){[]}{}(){[]}(){}[[]]()[[]]()()()[]()({{}})[]()((()))()([]){}[()][]{}()[](()){}[][[]][[]]{}()([])[]({})()[]{}({}){}()(){}()(()){}()(){}{}{}{[]}()[]()()[]{[]}(){}[[]]()[()][][][][[]][{}]({})({})(()){}{({})}{{}}{}[]{}[()]()(){}[]{[]}()[]()()()({{}})()(){}({()})[()](([()])){{{}}}(())[]{}(){}[[]][]{}[]{}[]{()}()[]{}()[]{}{}{}[]{}{{}}{()}[()]{{}}[]{{}}[]{}([]){}({}){}[]{}()()(){}()[]{{}}{}{}(([{}]))[{}]()[]{(())}[]()[]{}()()([])[()]()()()(){{}}()()()[[]]{}(){[]}[[]]({})[([[]])]{{{[]}}}[{([])}]({[{}]})[[]][]{{({})}}()[][{[]}]{}{({()})}{}[(())]{}[](){}()[]{}{}[]{}(()){}[[]]()()(){}{}(){{}}[]()(){}(){}[()](){}{}(){}()[()]()()[][][]{}[][[]]()[]{[]}[]{()}(){}()[]{}{}[()][]{{}}[[[]]][()]{}([{()}])[[[]]][]()(){}()()()()[[]][{}]{}{}[][[]][[]]{()}[({})]{}{{}}[]{{}}[()]{{}}[]{}({}){{}}{[]}(){}{}{}[][()][]()(){({})}(()){}[][][]{}{()}()[]({[{}]})[]{[{}]}[]{{}}{}{}([])()()()({}){}[]()[][{}]{{}}{}[](){}()({}){{[]}}()[]{}{}{}{()}{}{}(){}()({})[{}]{}[{}]()(()){}[]{()}[[()]]{}(){()}{}(){}{}()[]{}[]()[](){[]}[][{}]{{}}[](({}))[][()][]{{}}()(()){}[]{{}}[][]{}{}{}(){}{}{}()()(){}()[]({})()[{}]{}{()}[]{{{}}}[]()(()){[]}[{}]{}{}()[][]()(){}[][][]({})[]{}(){}{{}}(()){}()[]{}[{()}](([])){}()({})[()]()[][[()]][]{}{}{}[]{}[]{}()[]{()}(){}[]{}(){}()(){}[]()(){}(()){}(())[]([])[]{}[][]()[()]({({})}){(({}))}[][]{{({})} ... [{()}]}{{}}()[][]({(())})[]{}[()][[()]][]{}()(){}()()()()()(){}[]()[][](){}()(({[]}))({})[]({{({})}})[[]]({({[]})})([{(())}]){}{}{{}}[]([])[][]()[()]{}{}()[[]]()[](){[]}[][({[]})]{}{[]}[{}][][]{}{[[{}]]}(())[(({}))][]({})()({[]}){[]}[[()]][[]](){[]}[(([[]]))][][]()[][][][][]()[][[{{{}}}]][][]{}(([[()]])){[[]]}[[]](())(()){}[][([])]{}[][{}](){[]}({})(())(){}()[[]]()[[()]]{}([()])({()})(){({})}[][][]()[{}]()(){}()[][]()[]{}{}(){}{}(())[][]{}[][[]]()()[{}]()[][{}][]()[()]{[[[()]]]}{}{}[({{()}})]{}[][][]{}{}[{}][()]{}()[](){}()[()](){{}}{}[][][[[{}]]](){}{}[](())(){}{}()()(){{}}(){}[[]][()]{()}()((())){{}}()(){}()[]()()[][][][]{}[][]{}()[]()[]{}(){}{}([])([])(){[]}[{}]({}){}[][]()(())[][](){{}}{}{}{({})}{}[](({}))(){}{()}[][]([])(){[]}[]{}{()}(()){}()()[][{}]{}(){{}}{}{{}}()[{}](){}{}[[]][]{}()[]{}({{}})[({})]({})()[](){}([]){[()]}[{}](){{}}([[]])[[]][][]()[()]()[][][]{}{()}[][]({}){}{}[[]]{[]}[]{}{}[]{[]}{}[[[]]]{}[]()[]{}({[{()}]}){{[{(())}]}}[]{([[]])}[]()[()](){}[][]{(())}[]{}()[][{}][]{}(())(){[{[{}]}]}{}[]{}[({})]()()[]((({()})))(({})){}{()}()()()()()[]{}()(){()}([])[]{[([])]}[[{{[]}}]]{}{}{}[]{[]}[]{}[][]({})(({})){}[[()]][]{()}[][()][][]{{}}()[[]][{}]{}{}[]()[](){{}}{}[[]]{}[{}]({})[][]{}{}({})()[]{}{[]}[]{}[][]{}[]{}(){}()[]([])[][{}][]{{}}[{[]}]{}[(())](){}(){}((()))()()()()(){}{{}}()[](){}(())({()}){}[]{}(){}[()]{}((()))({})[()]{}{}{}({})[[]]{}(){}{}{}{{}}[]()[[]][]()[](){}[]{}[]()([]){}({})[{}]{}()[]([[[]]])[[[]]]()([])(){}[]{}()[]{}[]{{()}}([]){{{}}}[{}]{}{[]}[([])]{}[{}][[[]]]()()[{}]{}{}{}(){}()()()()[]()()({})([])()[][]{}()[()][]()[]{}(()){()}{()}{()}{}{}(){}[]{[]}[({})][][][]{}{}(){}(){}{}{}{()}{}(){}{}{}{}[()]{}{({})}[([])][]()()[{}](){}[[]][[[]]]{[]}{{}}()(([]))()(){}[][]()(){}()({})()([])()[][{}]()()([[[]]]){{[]}}[]({()}){[]}{[]}()[[]]{[]}[{}]{}{}[][]{}[{}][{}]{}[]{()}(){{()}}{{{}}}{}{}{}[()][]{}[()]()({{}}){[]}{({[]})}{}({}){}{}()[][()][]()(){}([])[]({{}})(())({})()(){}([]){}[[[]]](){}{}({})[]([])(){}(){[{}]}{[]}[{()}]{[]}{[]}[][]{}{}{[]}()()[]{{}}{}{[]}{}[{}][]{}({}){}[()]([])()[]{}{{}}()(())[[]](){}[](){{}}()()()[]()[](){}([()])()({()}){}({[{}]})[]{}([]){}[[]]{}{}[]()[(())][][][](())[([])](()){}{}{{[]}}{}()()[][]()({})[][]()(){}()[]({{}})(){[]}[]()[](())()(){}{()}({})[()](){{}}{()}()[]{[]}{{}}{{{}}}(){}{{}}{()}{()}[]{}()[]{()}(){}[]{}{}[]()";
+#else
+#define MAX_INPUT 	200000
 #endif  /* End of TEST */
-
-
-//#define MAX_INPUT 	100000
-#define MAX_INPUT 	20
-StackObj_T BracketStack;
-uint8_t output_str[MAX_INPUT]={0};
-char checkme[MAX_INPUT]="foo(bar);";
 
 /* Return 0 if success, or idx based 1 if there is error */
 uint32_t IsBalanced(char* p_data)
@@ -40,6 +78,7 @@ uint32_t IsBalanced(char* p_data)
 		if( (input_data[idx] == '(')  || (input_data[idx] == '[') || (input_data[idx] == '{'))
 		{
 			StackPush(&BracketStack, input_data[idx]);
+			StackPush(&PosStack, idx);
 		}
 		else if ( (input_data[idx] == ')') || (input_data[idx] == ']') || (input_data[idx] == '}') )
 		{
@@ -53,9 +92,22 @@ uint32_t IsBalanced(char* p_data)
 					( (input_data[idx] == '}') && (top != '{')  ) 	 )
 					return idx+1; /* Dong khong dung ngoac*/
 				else
+				{
 					StackPop(&BracketStack);
+					StackPop(&PosStack);
+				}
 			}
 		}
+	}
+	uint32_t last_index=0;
+	if(StackIsEmpty(&BracketStack) == false)	/* Mo khong dong */
+	{
+		while(StackIsEmpty(&PosStack) == false)
+		{
+			 last_index = StackGetTop(&PosStack);
+			 StackPop(&PosStack);
+		}
+		return last_index+1;
 	}
 	return 0;
 }
@@ -63,7 +115,15 @@ uint32_t IsBalanced(char* p_data)
 int main(void) {
 	setbuf(stdout, NULL);
 	StackInit(&BracketStack);
+	StackInit(&PosStack);
+	#if !TEST
+	char checkme[MAX_INPUT]={0};
+	gets(checkme);
 	uint32_t where = IsBalanced(checkme);
+	#else
+	uint32_t where = IsBalanced(input_data);
+	#endif  /* End of TEST */
+
 	if (where == 0)
 		printf("Success");
 	else
@@ -71,42 +131,220 @@ int main(void) {
 	return EXIT_SUCCESS;
 }
 
-
-#if TEST_STACK
-StackObj_T TestStackObj={0};
-void TestStack(void)
+void* LLGetFront(LLObject_T* Obj_P)
 {
-	StackInit(&TestStackObj);
-	if (StackIsEmpty(&TestStackObj) == true)
+	return Obj_P->Head_P->Key_P;
+}
+
+void* LLGetBack(LLObject_T* Obj_P)
+{
+	return Obj_P->Tail_P->Key_P;
+}
+
+void LLPushFront(LLObject_T* Obj_P, void* Key_P)
+{
+	myLL* p_newNode = (myLL*) malloc( sizeof(myLL) );
+	if (p_newNode == NULL)
 	{
-		StackPush(&TestStackObj, CountChar[0]);
-		StackPush(&TestStackObj, CountChar[1]);
-		g_checkme = StackGetTop(&TestStackObj);
-		g_checkme = StackPop(&TestStackObj);
-		g_checkme = StackPop(&TestStackObj);
-		g_checkme = StackPop(&TestStackObj);
+		printf("Cant allocate memory \n");
+		while(1);
+	}
+	p_newNode->Key_P = Key_P;
+	p_newNode->Next_P = Obj_P->Head_P;
+	Obj_P->Head_P = p_newNode;
+    if(Obj_P->Tail_P == NULL) Obj_P->Tail_P = Obj_P->Head_P;
+}
+
+void LLPushBack(LLObject_T* Obj_P, void* Key_P)
+{
+	myLL* p_newNode = (myLL*) malloc( sizeof(myLL) );
+	if (p_newNode == NULL)
+	{
+		printf("Cant allocate memory \n");
+		while(1);
+	}
+	p_newNode->Key_P = Key_P;
+	p_newNode->Next_P = NULL;
+	if(Obj_P->Head_P == NULL)
+	{
+		Obj_P->Head_P = p_newNode;
+		Obj_P->Tail_P = Obj_P->Head_P;
 	}
 	else
-		printf("What wrong with u \n");
+	{
+		Obj_P->Tail_P->Next_P = p_newNode;
+		Obj_P->Tail_P = p_newNode;
+	}
 }
-#endif  /* End of TEST_STACK */
 
-#if TEST_LL
-LLObject_T TestLLObj;
-void TestLL(void)
+void LLPopFront(LLObject_T* Obj_P)
 {
-	LLObject_T* P_TestLL = LLCreate();
-	LLPushFront(&TestLLObj, &CountChar[2]);
-	LLPrintList(&TestLLObj);
-	LLPushBack(&TestLLObj, &CountChar[0]);
-	LLPrintList(&TestLLObj);
-	LLPushBack(&TestLLObj, &CountChar[1]);
-	LLPushFront(&TestLLObj, &CountChar[3]);
-	LLPrintList(&TestLLObj);
-	LLPopBack(&TestLLObj);
-	LLPrintList(&TestLLObj);
-	LLPopFront(&TestLLObj);
-	LLPrintList(&TestLLObj);
-	LLPrintList(P_TestLL);
+	if(Obj_P->Head_P == NULL)
+	{
+		printf("Error: HeadPt Null ???\n");
+		while(1);
+	}
+	else
+	{
+		myLL *p_curhead = Obj_P->Head_P;
+		Obj_P->Head_P = Obj_P->Head_P->Next_P;
+		if(Obj_P->Head_P == NULL) Obj_P->Tail_P = NULL;
+		free(p_curhead);
+	}
 }
-#endif  /* End of TEST_LL */
+
+void LLPopBack(LLObject_T* Obj_P)
+{
+	if(Obj_P->Head_P == NULL)
+		printf("There is nothing to pop \n");
+	else
+	{
+		myLL* p_temp=Obj_P->Head_P;
+		if (Obj_P->Head_P == Obj_P->Tail_P)
+		{
+			Obj_P->Head_P = NULL;
+			Obj_P->Tail_P = NULL;
+		}
+		else
+		{
+			while(p_temp->Next_P != Obj_P->Tail_P)
+			{
+				p_temp = p_temp->Next_P;
+			}
+			p_temp->Next_P = NULL;	/* Update node before tail */
+			Obj_P->Tail_P = p_temp;
+			p_temp = p_temp->Next_P;
+		}
+		free(p_temp);
+	}
+}
+
+void LLPrintList(LLObject_T* Obj_P)
+{
+	printf("\n");
+	printf("HeadPt: %p \n", Obj_P->Head_P);
+	myLL* p_cur = Obj_P->Head_P;
+	printf("------------------------------------------------------- \n");
+	while(1)
+	{
+		if(p_cur != NULL)
+		{
+			printf("Address %p --- Value: %3d --- Next: %p \n", p_cur, *((uint8_t*)p_cur->Key_P), p_cur->Next_P);
+			p_cur = p_cur->Next_P;
+		}
+		else
+		{
+			break;
+		}
+	}
+	printf("------------------------------------------------------- \n");
+	printf("TailPt: %p \n",Obj_P->Tail_P);
+	printf("\n");
+}
+
+void LLInit(LLObject_T* Obj_P)
+{
+	#if 0
+	LL_P->GetFront	= &LLGetFront;
+	LL_P->GetBack	= &LLGetBack;
+	LL_P->PushFront	= &LLPushFront;
+	LL_P->PushBack	= &LLPushBack;
+	LL_P->PopFront	= &LLPopFront;
+	LL_P->PopBack	= &LLPopBack;
+	LL_P->IsEmpty	= &LLIsEmpty;
+	LL_P->IsExist	= &LLIsExist;
+	LL_P->PrintList	= &LLPrintList;
+	#endif  /* End of 0 */
+}
+
+bool LLIsEmpty(LLObject_T* Obj_P)
+{
+	if(Obj_P->Head_P == NULL)
+		return true;
+	else
+		return false;
+}
+
+bool LLIsExist(LLObject_T* Obj_P, void* Key_P)
+{
+	myLL* p_cur = Obj_P->Head_P;
+	while(p_cur != NULL)
+	{
+		if (p_cur->Key_P != Key_P)
+			p_cur = p_cur->Next_P;
+		else
+			return true;
+	}
+	return false;
+}
+
+LLObject_T* LLCreate(void)
+{
+	LLObject_T *p_ll = malloc(sizeof (LLObject_T));
+	if (p_ll == NULL)
+	{
+		while(1);
+	}
+	p_ll->Head_P= NULL;
+	p_ll->Tail_P= NULL;
+	return p_ll;
+}
+
+bool StackIsEmpty(StackObj_T* Obj_P)
+{
+	if(LLIsEmpty(Obj_P->StackLL) == true)
+		return true;
+	else
+		return false;
+}
+
+
+void StackPush(StackObj_T* Obj_P, uint8_t data)
+{
+	uint8_t* p_data = (uint8_t*) malloc( sizeof(data));
+	*p_data = data;
+	LLPushFront(Obj_P->StackLL, p_data);
+	Obj_P->numbElement++;
+}
+
+/*
+ * Return the top element without popping it out
+ */
+uint8_t StackGetTop(StackObj_T* Obj_P)
+{
+	uint8_t retval=0;
+	if( StackIsEmpty(Obj_P) == true)
+	{
+		printf("Stack is empty");
+	}
+	else
+	{
+		retval = *(uint8_t*)LLGetFront(Obj_P->StackLL);
+	}
+	return retval;
+}
+
+uint8_t StackPop(StackObj_T* Obj_P)
+{
+	uint8_t retval=0;
+	if (StackIsEmpty(Obj_P) == true)
+	{
+		printf("Stack is empty");
+	}
+	else
+	{
+		uint8_t* p_data = (uint8_t*) LLGetFront(Obj_P->StackLL);
+		retval = *p_data;
+		LLPopFront(Obj_P->StackLL);
+		free(p_data);
+		Obj_P->numbElement--;
+	}
+	return retval;
+}
+
+void StackInit(StackObj_T* Obj_P)
+{
+	Obj_P->StackLL = LLCreate();
+	Obj_P->numbElement = 0;
+}
+
